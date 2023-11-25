6fcc43c3811dd6d4e00d83c07c362e058d723006
Hopertz <lugano.ngulwa@gmail.com>
2023-11-25 07:43:57 +0300

    add b2bpayment for client

--- types.go
+++ types.go
@@ -13,6 +13,7 @@ const (
 	SandboxEndpoint = "/sandbox/ipg/v2/vodacomTZN/"
 	SessionEndPath  = "getSession"
 	C2BPaymentPath  = "c2bPayment/singleStage"
+	B2BPaymentPath  = "b2bPayment"
 )
 
 type (
@@ -45,6 +46,24 @@ type (
 	}
 
 	C2BPaymentResponse struct {
+		OutputResponseCode             string `json:"output_ResponseCode"`
+		OutputResponseDesc             string `json:"output_ResponseDesc"`
+		OutputTransactionID            string `json:"output_TransactionID"`
+		OutputConversationID           string `json:"output_ConversationID"`
+		OutputThirdPartyConversationID string `json:"output_ThirdPartyConversationID"`
+	}
+
+	B2BPaymentRequest struct {
+		InputAmount                   string `json:"input_Amount"`
+		InputReceiverPartyCode        string `json:"input_ReceiverPartyCode"`
+		InputCountry                  string `json:"input_Country"`
+		InputCurrency                 string `json:"input_Currency"`
+		InputPrimaryPartyCode         string `json:"input_PrimaryPartyCode"`
+		InputTransactionReference     string `json:"input_TransactionReference"`
+		InputThirdPartyConversationID string `json:"input_ThirdPartyConversationID"`
+		InputPurchasedItemsDesc       string `json:"input_PurchasedItemsDesc"`
+	}
+	B2BPaymentResponse struct {
 		OutputResponseCode             string `json:"output_ResponseCode"`
 		OutputResponseDesc             string `json:"output_ResponseDesc"`
 		OutputTransactionID            string `json:"output_TransactionID"`
